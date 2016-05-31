20706b949300f4945664e93c0fb2222e12d9641c
Clint <clint@macbook-air.local>
2016-05-30 18:25:51 -0700

    first git commit

--- t5/FirstViewController.swift
+++ t5/FirstViewController.swift
@@ -13,6 +13,7 @@ class FirstViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        println("testing github commit")
     }
 
     override func didReceiveMemoryWarning() {
